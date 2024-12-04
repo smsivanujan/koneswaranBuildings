e51fe23ff32bf716f72d2873124728288bc9bbcb
sivanujan <smsivanujan@gmail.com>
2024-12-04 20:51:21 +0530

    Signed-off-by: sivanujan <smsivanujan@gmail.com>

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -8,7 +8,7 @@
 			<div class="container">
 				<div class="main-sidemenu navbar px-0">
 					<a class="navbar-brand ps-0 d-none d-lg-block" href="/">
-						<img alt="" class="logo-2" src="../assets/images/brand/logo-3.png" style="width: 104px; height: 36px;">
+						<img alt="" class="logo-2" src="../assets/images/brand/logo-3.png" style="width: 50px; height: 50px;">
 						<img alt="" class="dark-landinglogo" src="../assets/images/brand/logo.png">
 					</a>
 					<ul class="side-menu">
